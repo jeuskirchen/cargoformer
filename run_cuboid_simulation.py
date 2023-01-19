f1ec2c4b32830ecd7432159c484938fc4caaa93c
jeuskirchen <jeuskirchen@protonmail.com>
2023-01-19 20:03:00 +0100

    add more methods to generate layouts

--- run_cuboid_simulation.py
+++ run_cuboid_simulation.py
@@ -1,5 +1,6 @@
 import pybullet as p
 import pybullet_data
+from squaternion import Quaternion
 from time import sleep
 import datetime as dt
 import numpy as np
@@ -7,19 +8,19 @@ from typing import List
 import sys
 
 
-# MODE = p.GUI
-MODE = p.DIRECT
+MODE = p.GUI
+# MODE = p.DIRECT
 SAVE = True
 TIMESTEP = 1/120
 NUM_STEPS = 500
 SAVE_EVERY_STEPS = 1
-NUM_ITEMS = 10
+NUM_ITEMS = 100
 # NUM_SIMULATIONS = 1
 NUM_SIMULATIONS = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 ORIENTATION_NOISE = 0.01
 
-"""
-PHOTO_EVERY_STEPS = 100
+TAKE_PHOTOS = False
+PHOTO_EVERY_STEPS = 10
 view_matrix = p.computeViewMatrix(cameraEyePosition=[1, 1, 1],
                                   cameraTargetPosition=[0, 0, 0],
                                   cameraUpVector=[0, 0, 1])
@@ -37,63 +38,82 @@ projection_matrix = p.computeProjectionMatrixFOV(fov=fov,
                                                  aspect=aspect,
                                                  nearVal=near_val,
                                                  farVal=far_val)
-"""
+
+
+def generate_random_item():
+    dim = np.random.uniform(0.1, 0.5, size=3).round(4)
+    pos = [*np.random.uniform(-3.0, 3.0, size=2).round(4),
+           max(np.round(np.random.uniform(0, 3.0), 4), dim[2]/2+0.01)]
+    # Making sure that the pos_height is at least the dim_height/2, otherwise the item is stuck in the ground
+    # re-order so it's the same ordering as in pybullet; only if I use squaternion above!
+    # each item's vector looks like this: [x, y, z, w, d, h, q1, q2, q3, q4, m]
+    # [x, y, z] is the position, [w, d, h] is the shape
+    # Orientation
+    q = np.array(Quaternion.from_euler(0., 0., 0.))
+    q = q[[1, 2, 3, 0]]  # re-order so it's the same ordering as in pybullet; only if I use squaternion above!
+    # add a little bit of noise to the orientation
+    noise = np.random.normal(0, ORIENTATION_NOISE, size=4)
+    q = (q + noise).round(4)
+    orn = q
+    # for now, assume zero orientation (they're all cuboids; I can change orientation by changing its dimensions)
+    # for Euler orientation I would use notation [a, b, g] (alpha, beta, gamma)
+    # Mass
+    # to make things easy, mass is always simply proportional to volume and this proportionality coefficient is
+    # randomly sampled to be between, say, 5 and 15, according to a uniform distribution
+    m = (np.prod(dim)*np.random.uniform(5, 15)).round(4)
+    # Each item's vector looks like this: [x, y, z, w, d, h, q1, q2, q3, q4, m]
+    return np.array([*pos, *dim, *orn, m])
+
+
+def intersects(item1, item2):
+    # True if item1 and item2 intersect
+    # Note that w, h, d are half-extents!!
+    x1, y1, z1, w1, d1, h1, _, _, _, _, _ = item1
+    x2, y2, z2, w2, d2, h2, _, _, _, _, _ = item2
+    # Just to make sure that they are also not too close to each other, add a small "margin" to each item
+    # only for comparison purposes
+    w1, d1, h1 = np.array([w1, d1, h1]) + 0.01
+    w2, d2, h2 = np.array([w2, d2, h2]) + 0.01
+    min_x1, max_x1, min_y1, max_y1, min_z1, max_z1 = x1-w1, x1+w1, y1-d1, y1+d1, z1-h1, z1+h1
+    min_x2, max_x2, min_y2, max_y2, min_z2, max_z2 = x2-w2, x2+w2, y2-d2, y2+d2, z2-h2, z2+h2
+    return ((min_x1 < min_x2 < max_x1) or (min_x2 < min_x1 < max_x2)) and \
+           ((min_y1 < min_y2 < max_y1) or (min_y2 < min_y1 < max_y2)) and \
+           ((min_z1 < min_z2 < max_z1) or (min_z2 < min_z1 < max_z2))
+
+
+def any_intersects(item1, items):
+    for item in items:
+        if intersects(item1, item):
+            return True
+        return False
 
 
 def generate_random_example() -> List:
-    """
-    List of items in one particular simulation
-    FIXME: Make sure the items don't intersect (!!!)
-           Perhaps generate N items independently and then remove those that intersect with any of the other items
-           Or alternatively, iteratively add more items, and with each new one make sure it has valid
-           (non-intersecting) position and dimensions.
-    """
     items = []
     for _ in range(NUM_ITEMS):
-        dim = np.random.uniform(0.1, 0.5, size=3).round(4)
-        pos = [*np.random.uniform(-3.0, 3.0, size=2).round(4),
-               max(np.round(np.random.uniform(0, 3.0), 4), dim[2]/2+0.01)]
-        # Making sure that the pos_height is at least the dim_height/2, otherwise the item is stuck in the ground
-        # q = p.getQuaternionFromEuler(np.random.uniform(0.0, 2*np.pi, size=3))  # random orientation
-        q = p.getQuaternionFromEuler([0., 0., 0.])
-        # add a little bit of noise to the orientation
-        noise = np.random.normal(0, ORIENTATION_NOISE, size=4)
-        q = (q + noise).round(4)
-        orn = q
-        """
-        Alternative to p.getQuaternionFromEuler, I can use squaternion:
-        q = np.array(Quaternion.from_euler(0., 0., 0.))
-        orn = q[[1, 2, 3, 0]]  # re-order so it's the same ordering as in pybullet; only if I use squaternion above!
-        """
-        # for now, assume zero orientation (they're all cuboids; I can change orientation by changing its dimensions)
-        m = np.prod(dim).round(4)*10  # for now, mass is always simply proportional to volume
-        items.append([*pos, *dim, *orn, m])
-        # each item's vector looks like this: [x, y, z, w, d, h, q1, q2, q3, q4, m]
-        # [x, y, z] is the position, [w, d, h] is the shape, [q1, q2, q3, q4] is the orientation as quaternions,
-        # m is the mass
-        # for Euler orientation I would use notation [a, b, g] (alpha, beta, gamma)
-    """
-    # Hardcoded example:
-    # (-0.1324, 0.0, 0.0, 1.0) is simply quaternion orientation of Euler (50.0, 0.0, 0.0)
-    items = [
-        [1.0, 1.0,  2.0,   1.0, 1.0, 1.0,   0.0000, 0.0, 0.0, 1.0,   0.5],
-        [1.0, 1.0,  5.0,   1.0, 1.0, 1.0,   0.0000, 0.0, 0.0, 1.0,   1.0],
-        [1.0, 1.5, 10.0,   0.5, 3.0, 1.0,  -0.1324, 0.0, 0.0, 1.0,   3.0],
-    ]
-    """
+        items.append(generate_random_item())
     return items
 
 
 def generate_realistic_example() -> List:
-    """
-    Generates an example layout of items.
-    """
-    pass
+    # TODO: Check if it's even possible to have an arrangement with this many items in the expected space
+    #  Since the position is sampled from a normal distribution, it might eventually randomly sample a box
+    #  that's far enough not to intersect
+    items = []
+    for _ in range(NUM_ITEMS):
+        proposed_item = generate_random_item()
+        if i > 0:
+            while any_intersects(proposed_item, items):
+                print("trying again")
+                proposed_item = generate_random_item()
+        items.append(proposed_item)
+        print(proposed_item)
+    return items
 
 
 def run_simulation() -> np.array:
     timestamp = str(dt.datetime.now()).replace(" ", "_").replace(":", "_").replace("-", "_").replace(".", "_")
-    data = generate_random_example()
+    data = generate_realistic_example()
 
     print("Setting up physics engine")
     p.connect(MODE)
@@ -133,8 +153,7 @@ def run_simulation() -> np.array:
                 timestep_data.append([*pos, w, d, h, *orn, m])
                 # print(t, (pos, orn))
             simulation_data.append(timestep_data)
-        """
-        if t % PHOTO_EVERY_STEPS == 0:
+        if TAKE_PHOTOS & t % PHOTO_EVERY_STEPS == 0:
             _, _, rgb_img, dep_img, seg_img = p.getCameraImage(width=view_width,
                                                                height=view_height,
                                                                viewMatrix=view_matrix,
@@ -150,7 +169,6 @@ def run_simulation() -> np.array:
                 np.save(open(f"screenshots/{timestamp}_t_{t}_rgb.npy", "wb"), rgb_img)
                 np.save(open(f"screenshots/{timestamp}_t_{t}_dep.npy", "wb"), dep_img)
                 np.save(open(f"screenshots/{timestamp}_t_{t}_seg.npy", "wb"), seg_img)
-        """
         if MODE == p.GUI:
             sleep(1/240)
         # check if stuff is changing, if not, break from loop
